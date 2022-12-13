1c78537f64b0c6848a674d00d1f49359b4526081
ThomasCode92 <thomascode92@hotmail.com>
2022-12-13 12:38:20 +0100

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
