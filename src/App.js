ae41db49de5601dc999bd8141554af9c38e8ac52
ThomasCode92 <thomascode92@hotmail.com>
2022-12-14 12:32:48 +0100

    Component State

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Thomas',
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
+        <p>Hi {this.state.name}</p>
+        <button>Change Name</button>
       </div>
     );
   }
