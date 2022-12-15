fb8ef795db39b3fdcfa29338ecac5a9b3ae2cda7
ThomasCode92 <thomascode92@hotmail.com>
2022-12-15 13:08:55 +0100

    Searching & Filtering

--- src/App.js
+++ src/App.js
@@ -31,7 +31,16 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={event => {}}
+          onChange={event => {
+            const inputValue = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter(monster =>
+              monster.name.toLocaleLowerCase().includes(inputValue)
+            );
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
         />
         {this.state.monsters.map(monster => {
           return (
