ee61c67519f6aa79441b59308fa34751bbbc8199
ThomasCode92 <thomascode92@hotmail.com>
2022-12-20 12:32:29 +0100

    Hooks: useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
@@ -8,21 +8,25 @@ import './App.css';
 const App = () => {
   const [searchString, setSearchString] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  console.log('render');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setMonsters(users));
+  });
 
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => setMonsters(users));
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster =>
+      monster.name.toLocaleLowerCase().includes(searchString)
+    );
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchString]);
 
   function onSearchChange(event) {
     const inputValue = event.target.value.toLocaleLowerCase();
     setSearchString(inputValue);
   }
-
-  const filteredMonsters = monsters.filter(monster =>
-    monster.name.toLocaleLowerCase().includes(searchString)
-  );
 
   return (
     <div className="App">
