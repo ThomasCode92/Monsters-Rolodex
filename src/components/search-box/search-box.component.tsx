d399d687c2769cc2951c760791ec9bfef5951f3f
ThomasCode92 <thomascode92@hotmail.com>
2022-12-22 12:43:30 +0100

    Typescript Interfaces

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,6 +1,19 @@
 import './search-box.styles.css';
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+interface ISearchBoxProps extends IChangeHandlerProps {
+  className: string;
+  placeholder: string;
+}
+
+interface IChangeHandlerProps {
+  onChangeHandler: (a: string) => void;
+}
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: ISearchBoxProps) => {
   return (
     <input
       className={`search-box ${className}`}
